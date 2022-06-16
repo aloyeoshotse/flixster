2adc6e81e8a227fcae3435b8b258367eaffeb785
Aloye Oshotse <aoshotse@aoshots-ltmrbwu.internal.salesforce.com>
2022-06-16 15:22:53 -0700

    completed displayMovies() and added some CSS styles

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
+//differnt endpoint for now playing vs search vs other stuff
+//config endpoint --> (look this up)
+
 //DOM Variables
 const form = document.querySelector("#search");
 const userInput = document.querySelector("#search-input");
 const submitButton = document.querySelector("#search-btn");
 const closeSubmitButton = document.querySelector("#close-search-btn");
 const movieGrid = document.querySelector("#movie-grid");
-//const movieCard = document.querySelector(".movie-card");
 const movieTitle = document.querySelector(".movie-title");
 const moviePoster = document.querySelector("#movie-poster");
 const movieVotes = document.querySelector(".movie-votes");
 const loadMoreMovies = document.querySelector("#load-more-movies-btn");
 
-//Parameters for Movie Database API
+//Parameters for Movie Database API 
 //https://developers.themoviedb.org/3/search/search-movies --> movie database source
 const api = "517461a5845ab1a8b80623111b4006fc"; //api key from the MOVIE DB
-var userInputElem;
+var userInputElem; //user input into search bar
 //var year; //year the movie was released
 
 //Filter Variables
@@ -32,6 +34,7 @@ async function getMovieData(input) {
         var resData = await data.json(); //converting data into something readable for JavaScript
         console.log(data);
         console.log(resData);
+        
         displayMovie(resData);
     }
     catch(error) {
@@ -39,9 +42,24 @@ async function getMovieData(input) {
     }
 }
 
-function displayMovie(responseData) {
-    /*this function takes the api data in json format and uses createMovieTemplate() to generate
-    a movie template that will be displayed on the webpage*/
+function displayMovies(responseData) {
+    /*this function takes the api data in json format and generates
+    a movie template that will be displayed on the webpage by adding 
+    to the html*/
+    const movieLink = "https://www.themoviedb.org/t/p/w440_and_h660_face"; //movie DB image link
+    responseData.results.map((item) => {
+        movieGrid.innerHTML += `
+        <div class="movie-card">
+            <img id="movie-poster" src=${movieLink + item.poster_path}>
+            <div class="movie-title">${item.title}</div>
+            <div class="movie-votes">${item.vote_average}</div>
+        </div>
+        `
+       /* console.log(item.title)
+        console.log(typeof(item.poster_path));
+        console.log(item.poster_path);
+        */
+    })
 }
 
 function handleFormSubmit(event) {
