469bb89e0d354cd77780a459a9bb7b818fc24856
Aloye Oshotse <aoshotse@aoshots-ltmrbwu.internal.salesforce.com>
2022-06-17 18:31:16 -0700

    added close search feature

--- script.js
+++ script.js
@@ -24,16 +24,17 @@ var userInputElem; //user input into search bar
 //var end_date;
 //var movie_id;
 
+
+//functions to load main page
 async function makeCurrentPage(loadevent) {
     loadevent.preventDefault();
     let curr_url = "https://api.themoviedb.org/3/movie/now_playing?api_key=517461a5845ab1a8b80623111b4006fc&language=en-US&page=1"; //movie database url with unique api key
     let curr_data = await fetch(curr_url); //fetching the data from the databse using the url
     var curr_resData = await curr_data.json(); //converting data into something readable for JavaScript
     currDisplayMovies(curr_resData);
-    console.log(curr_data);
-    console.log(curr_resData);
+    // console.log(curr_data);
+    // console.log(curr_resData);
 }
-
 
 function currDisplayMovies(responseData) {
     /*this function takes the api data in json format and generates
@@ -51,6 +52,8 @@ function currDisplayMovies(responseData) {
     }) 
 }
 
+
+//functions for search input
 async function getMovieData(input) {
     /*this function fetches uses the user input in order to fetch the search term from the 
     movie database api and converts it into a json which is passed into displayMovie()*/
@@ -60,7 +63,6 @@ async function getMovieData(input) {
         var resData = await data.json(); //converting data into something readable for JavaScript
         console.log(data);
         console.log(resData);
-        
         displayMovies(resData);
     }
     catch(error) {
@@ -72,9 +74,11 @@ function displayMovies(responseData) {
     /*this function takes the api data in json format and generates
     a movie template that will be displayed on the webpage by adding 
     to the html*/
+    closeSubmitButton.removeAttribute("hidden"); //this closes out the search
+    loadMoreMovies.removeAttribute("hidden")
     const movieLink = "https://www.themoviedb.org/t/p/w440_and_h660_face"; //movie DB image link
+    movieGrid.innerHTML = ``;
     responseData.results.map((item) => {
-        movieGrid.innerHTML = ``;
         movieGrid.innerHTML += `
         <div class="movie-card">
             <img id="movie-poster" src=${movieLink + item.poster_path}>
@@ -82,10 +86,9 @@ function displayMovies(responseData) {
             <div class="movie-votes">🌟${item.vote_average}</div>
         </div>
         `
-       /* console.log(item.title)
+        console.log(item.title)
         console.log(typeof(item.poster_path));
         console.log(item.poster_path);
-        */
     })
 }
 
@@ -94,6 +97,18 @@ function handleFormSubmit(event) {
     userInputElem = encodeURI(userInput.value);
     console.log(userInput);
     getMovieData(userInputElem);
+}
+
+function removeHiddenX() {
+    //removes the X button when it is clicked
+    window.location.reload();
+    closeSubmitButton.hidden = true; 
+    //form.addEventListener('click',(clickevent) => {makeCurrentPage(clickevent);})
+}
+
+function removeHiddenMore() {
+    //removes the more button when it is clicked
+    loadMoreMovies.hidden = true;
 }
 
 
